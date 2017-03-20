50d11fc0887aaadf61528b45746aa1206da9e48e
Julius Te <mail@juliuste.de>
2017-03-20 01:11:30 +0100

    make require static again

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 'use strict'
 
-const ebenen = ['staat', 'länder', 'regierungsbezirke', 'kreise', 'verwaltungsgemeinschaften', 'gemeinden']
+const staat = require('./staat.geo.json')
+const laender = require('./länder.geo.json')
+const regierungsbezirke = require('./regierungsbezirke.geo.json')
+const kreise = require('./kreise.geo.json')
+const verwaltungsgemeinschaften = require('./verwaltungsgemeinschaften.geo.json')
+const gemeinden = require('./gemeinden.geo.json')
 
-const e = {}
-
-for(let ebene of ebenen) e[ebene] = require(`./${ebene}.geo.json`)
-
-module.exports = e
+module.exports = {
+	staat,
+	'länder': laender,
+	regierungsbezirke,
+	kreise,
+	verwaltungsgemeinschaften,
+	gemeinden
+}
